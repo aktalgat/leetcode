919f1c9e4f8cd367ccd0dab8ff78c12f5f54975a
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2021-09-03 14:46:04 +0300

    Two sum

---
+++ src/io/talgat/easy/TwoSum.java
@@ -0,0 +1,20 @@
+package io.talgat.easy;
+
+/**
+ * https://leetcode.com/problems/two-sum/
+ */
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] a = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; j < nums.length; j++) {
+                if ((i != j) && (nums[i] + nums[j] == target)) {
+                    a[0] = i;
+                    a[1] = j;
+                    return a;
+                }
+            }
+        }
+        return a;
+    }
+}
