ec14d2808b9767defeb84bcb53d96245217b533a
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2021-09-08 14:45:10 +0300

    Middle of the Linked List

---
+++ src/io/talgat/easy/MiddleOfLinkedList.java
@@ -0,0 +1,25 @@
+package io.talgat.easy;
+
+/**
+ *
+ */
+public class MiddleOfLinkedList {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
