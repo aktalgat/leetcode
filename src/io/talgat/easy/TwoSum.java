34534eddc36cb8d073b580c8a6b05e4e8b90e2c3
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2025-10-19 23:59:53 +0300

    Two number sum in array equal target with map

--- src/io/talgat/easy/TwoSum.java
+++ src/io/talgat/easy/TwoSum.java
@@ -1,9 +1,21 @@
 package io.talgat.easy;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/two-sum/
  */
 public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] a = new int[] { 3, 2, 4 };
+        int target = 6;
+        TwoSum sut = new TwoSum();
+        System.out.println("Res: " + Arrays.toString(sut.twoSum2(a, target)));
+    }
+
     public int[] twoSum(int[] nums, int target) {
         int[] a = new int[2];
         for (int i = 0; i < nums.length; i++) {
@@ -16,5 +28,16 @@ public class TwoSum {
             }
         }
         return a;
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[] { map.get(target - nums[i]), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] { };
     }
 }
