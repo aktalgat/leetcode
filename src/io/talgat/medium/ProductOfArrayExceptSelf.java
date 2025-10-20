13528011cffdb40e565d2b8c37ac9fa9860cd7bf
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2025-10-20 14:14:55 -0500

    Product of Array Except Self

---
+++ src/io/talgat/medium/ProductOfArrayExceptSelf.java
@@ -0,0 +1,31 @@
+package io.talgat.medium;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/product-of-array-except-self
+ */
+public class ProductOfArrayExceptSelf {
+
+    public static void main(String[] args) {
+        ProductOfArrayExceptSelf sut = new ProductOfArrayExceptSelf();
+        //int[] input = new int[] { 1, 2, 3, 4 };
+        int[] input = new int[] { -1, 1, 0, -3, 3 };
+        System.out.println("Result: " + Arrays.toString(sut.productExceptSelf(input)));
+    }
+
+    public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        result[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = nums.length - 1; i >=0;  i--) {
+            result[i] = result[i] * right;
+            right *= nums[i];
+        }
+
+        return result;
+    }
+}
