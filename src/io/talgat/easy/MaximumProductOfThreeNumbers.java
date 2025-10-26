3127a9d12dfd6f22bb716c99cda80e76b39fa6b9
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2025-10-26 17:11:36 -0500

    Maximum Product of Three Numbers

---
+++ src/io/talgat/easy/MaximumProductOfThreeNumbers.java
@@ -0,0 +1,34 @@
+package io.talgat.easy;
+
+/**
+ * https://leetcode.com/problems/maximum-product-of-three-numbers
+ */
+public class MaximumProductOfThreeNumbers {
+
+    public int maximumProduct(int[] nums) {
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        for (int n : nums) {
+            if (n > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (n > max2) {
+                max3 = max2;
+                max2 = n;
+            } else if (n > max3) {
+                max3 = n;
+            }
+            if (n < min1) {
+                min2 = min1;
+                min1 = n;
+            } else if (n < min2) {
+                min2 = n;
+            }
+        }
+        long prod1 = (long) max1 * max2 * max3;
+        long prod2 = (long) max1 * min1 * min2;
+
+        return (int) Math.max(prod1, prod2);
+    }
+}
