cc58022e2c7dd5010b2c9f60a1d7e67d9a8796eb
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2025-10-28 21:02:09 -0500

    Pascal's Triangle

---
+++ src/io/talgat/easy/PascalTriangle.java
@@ -0,0 +1,26 @@
+package io.talgat.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/pascals-triangle
+ */
+public class PascalTriangle {
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
+                }
+            }
+            result.add(row);
+        }
+        return result;
+    }
+}
