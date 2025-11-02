671aa0f8815ca524b2bf4926eefded9f8a5ce30b
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2025-11-02 11:15:07 -0600

    Lucky Numbers in a Matrix

---
+++ src/io/talgat/easy/LuckyNumbersInMatrix.java
@@ -0,0 +1,36 @@
+package io.talgat.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LuckyNumbersInMatrix {
+
+    public List<Integer> luckyNumbers(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        int[] rowMin = new int[matrix.length];
+        int[] colMax = new int[matrix[0].length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] < min) min = matrix[i][j];
+            }
+            rowMin[i] = min;
+        }
+        for (int j = 0; j < matrix[0].length; j++) {
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < matrix.length; i++) {
+                if (matrix[i][j] > max) max = matrix[i][j];
+            }
+            colMax[j] = max;
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == rowMin[i] && matrix[i][j] == colMax[j]) {
+                    result.add(matrix[i][j]);
+                }
+            }
+        }
+        return result;
+    }
+}
