79fa98f8c5ddd1c6836e04c623674d7c7271b8e7
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2025-11-11 18:32:21 -0600

    Is Subsequence

---
+++ src/io/talgat/easy/IsSubsequence.java
@@ -0,0 +1,18 @@
+package io.talgat.easy;
+
+/**
+ * https://leetcode.com/problems/is-subsequence
+ */
+public class IsSubsequence {
+
+    public boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        return i == (s.length() - 1);
+    }
+}
