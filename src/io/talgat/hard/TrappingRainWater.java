dd2ad34bfd9142c335daed6722823f185b8a0cf2
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2025-11-12 17:54:04 -0600

    Trapping Rain Water

---
+++ src/io/talgat/hard/TrappingRainWater.java
@@ -0,0 +1,32 @@
+package io.talgat.hard;
+
+/**
+ * https://leetcode.com/problems/trapping-rain-water
+ */
+public class TrappingRainWater {
+
+    public int trap(int[] height) {
+        int water = 0;
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    water += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    water += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+        return water;
+    }
+}
