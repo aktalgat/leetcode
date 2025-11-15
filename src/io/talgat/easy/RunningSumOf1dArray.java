bba69c8bc36c3d392c8c1a7942560f825e059604
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2025-11-14 19:12:33 -0600

    Running Sum of 1d Array

---
+++ src/io/talgat/easy/RunningSumOf1dArray.java
@@ -0,0 +1,16 @@
+package io.talgat.easy;
+
+/**
+ * https://leetcode.com/problems/running-sum-of-1d-array
+ */
+public class RunningSumOf1dArray {
+
+    public int[] runningSum(int[] nums) {
+        int[] res = new int[nums.length];
+        res[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            res[i] = res[i - 1] + nums[i];
+        }
+        return res;
+    }
+}
