74e6ffa0aaee0033692203d1685ae00d170f743e
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2025-11-16 20:07:58 -0600

    Remove Linked List Elements

---
+++ src/io/talgat/easy/RemoveLinkedListElements.java
@@ -0,0 +1,22 @@
+package io.talgat.easy;
+
+/**
+ * https://leetcode.com/problems/remove-linked-list-elements
+ */
+public class RemoveLinkedListElements {
+
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode p = dummy;
+        while (p.next != null) {
+            if (p.next.val == val) {
+                p.next = p.next.next;
+            } else {
+                p = p.next;
+            }
+        }
+        return dummy.next;
+    }
+}
