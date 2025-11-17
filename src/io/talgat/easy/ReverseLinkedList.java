c3f9c51fb1fa1f1d2c33382a94ce0d8f32d615c3
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2025-11-16 20:13:35 -0600

    Reverse Linked List

---
+++ src/io/talgat/easy/ReverseLinkedList.java
@@ -0,0 +1,19 @@
+package io.talgat.easy;
+
+/**
+ * https://leetcode.com/problems/reverse-linked-list
+ */
+public class ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
