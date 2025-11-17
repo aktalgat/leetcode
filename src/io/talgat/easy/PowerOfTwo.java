406a8a41ed866168ba00c5be1b635ce7880c7b0f
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2025-11-17 14:44:29 -0600

    Power of Two

---
+++ src/io/talgat/easy/PowerOfTwo.java
@@ -0,0 +1,12 @@
+package io.talgat.easy;
+
+/**
+ * https://leetcode.com/problems/power-of-two
+ */
+public class PowerOfTwo {
+
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        return (n & (n - 1)) == 0;
+    }
+}
