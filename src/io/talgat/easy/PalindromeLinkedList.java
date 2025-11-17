90a95672e46ad887390ccbef3482274cfc0a85d5
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2025-11-17 14:51:44 -0600

    Palindrome Linked List

---
+++ src/io/talgat/easy/PalindromeLinkedList.java
@@ -0,0 +1,34 @@
+package io.talgat.easy;
+
+/**
+ * https://leetcode.com/problems/palindrome-linked-list
+ */
+public class PalindromeLinkedList {
+
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode prev = null;
+        ListNode curr = slow;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        ListNode first = head;
+        ListNode second = prev;
+        while (second != null) {
+            if (first.val != second.val) {
+                return false;
+            }
+            first = first.next;
+            second = second.next;
+        }
+        return true;
+    }
+}
