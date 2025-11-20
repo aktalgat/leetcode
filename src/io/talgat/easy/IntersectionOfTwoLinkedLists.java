d74d1f030c33ec7d43745d4aa8673fe7665488f8
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2025-11-20 16:32:04 -0600

    Intersection of Two Linked Lists

---
+++ src/io/talgat/easy/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,18 @@
+package io.talgat.easy;
+
+/**
+ * https://leetcode.com/problems/intersection-of-two-linked-lists
+ */
+public class IntersectionOfTwoLinkedLists {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode a = headA;
+        ListNode b = headB;
+        while (a != b) {
+            a = (a == null) ? headB : a.next;
+            b = (b == null) ? headA : b.next;
+        }
+        return a;
+    }
+}
