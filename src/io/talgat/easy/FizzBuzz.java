bcdd1eee2166e5346c3a5b5616e6488712f88bfa
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2025-11-22 12:14:34 -0600

    Fizz Buzz

---
+++ src/io/talgat/easy/FizzBuzz.java
@@ -0,0 +1,25 @@
+package io.talgat.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/fizz-buzz
+ */
+public class FizzBuzz {
+
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            StringBuilder sb = new StringBuilder();
+            if (i % 3 == 0 || i % 5 == 0) {
+                if (i % 3 == 0) sb.append("Fizz");
+                if (i % 5 == 0) sb.append("Buzz");
+            } else {
+                sb.append(i);
+            }
+            list.add(sb.toString());
+        }
+        return list;
+    }
+}
