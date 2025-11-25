baa62414018d0abe318a5a4e4b138460a68b5062
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2025-11-25 17:22:10 -0600

    Maximum Ascending Subarray Sum

---
+++ src/io/talgat/easy/MaximumAscendingSubarraySum.java
@@ -0,0 +1,21 @@
+package io.talgat.easy;
+
+/**
+ * https://leetcode.com/problems/maximum-ascending-subarray-sum
+ */
+public class MaximumAscendingSubarraySum {
+
+    public int maxAscendingSum(int[] nums) {
+        int maxSum = nums[0];
+        int sum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                sum += nums[i];
+            } else {
+                maxSum = Math.max(maxSum, sum);
+                sum = nums[i];
+            }
+        }
+        return Math.max(maxSum, sum);
+    }
+}
