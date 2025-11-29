1c2a184c26ca3b8aaafad3424e45c556b8d3c31b
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2025-11-29 17:19:56 -0600

    Transpose Matrix

---
+++ src/io/talgat/easy/TransposeMatrix.java
@@ -0,0 +1,19 @@
+package io.talgat.easy;
+
+/**
+ * https://leetcode.com/problems/transpose-matrix
+ */
+public class TransposeMatrix {
+
+    public int[][] transpose(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] result = new int[cols][rows];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[j][i] = matrix[i][j];
+            }
+        }
+        return result;
+    }
+}
