ab0f58b47b4622b89721f359c066aabe8a68c786
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2025-12-03 17:20:29 -0600

    Find the Town Judge

---
+++ src/io/talgat/easy/FindTownJudge.java
@@ -0,0 +1,24 @@
+package io.talgat.easy;
+
+/**
+ * https://leetcode.com/problems/find-the-town-judge
+ */
+public class FindTownJudge {
+
+    public int findJudge(int n, int[][] trust) {
+        if (n == 1) return 1;
+        int[] in = new int[n + 1];
+        int[] out = new int[n + 1];
+        for (int[] t : trust) {
+            int a = t[0], b = t[1];
+            in[b]++;
+            out[a]++;
+        }
+        for (int i = 0; i <= n; i++) {
+            if (in[i] == n - 1 && out[i] == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
