e890a8ed9dcdfa5a24c9a1b4d205a36133b1724b
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2025-12-13 11:49:53 -0600

    Make ListNode public

---
+++ src/io/talgat/easy/ListNode.java
@@ -0,0 +1,17 @@
+package io.talgat.easy;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
