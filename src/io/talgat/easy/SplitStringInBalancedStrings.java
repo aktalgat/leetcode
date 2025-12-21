90fcb71a7b5ced865614ca7e2d1e7c7dcf2c4b9d
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2025-12-20 18:18:58 -0600

    Split a String in Balanced Strings

---
+++ src/io/talgat/easy/SplitStringInBalancedStrings.java
@@ -0,0 +1,23 @@
+package io.talgat.easy;
+
+/**
+ * https://leetcode.com/problems/split-a-string-in-balanced-strings
+ */
+public class SplitStringInBalancedStrings {
+
+    public int balancedStringSplit(String s) {
+        int balance = 0;
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (c == 'L') {
+                balance++;
+            } else {
+                balance--;
+            }
+            if (balance == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
