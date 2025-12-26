7806a1fd0900372420ee7350d71ca6b3d2ef8fb6
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2025-12-25 19:15:24 -0600

    4Sum

---
+++ src/io/talgat/medium/FourSum.java
@@ -0,0 +1,43 @@
+package io.talgat.medium;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/4sum
+ */
+public class FourSum {
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        if (nums.length < 4) return List.of();
+        List<List<Integer>> result = new ArrayList<>();
+        int left = 0;
+        int right = 0;
+        long sum = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                left = j + 1;
+                right = nums.length - 1;
+                while (left < right) {
+                    sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum < target) {
+                        left++;
+                    } else if (sum > target) {
+                        right--;
+                    } else if (sum == target) {
+                        result.add(List.of(nums[i], nums[j], nums[left], nums[right]));
+                        left++;
+                        right--;
+                        while (left < right && nums[left] == nums[left - 1]) left++;
+                        while (left < right && nums[right] == nums[right + 1]) right--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
