e8c3ec02c3eb55ff2265865aebbd177ca65f6550
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2026-01-30 21:33:48 -0600

    Climbing Stairs

---
+++ src/io/talgat/easy/ClimbingStairs.java
@@ -0,0 +1,20 @@
+package io.talgat.easy;
+
+/**
+ * https://leetcode.com/problems/climbing-stairs
+ */
+public class ClimbingStairs {
+
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i < n + 1; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
