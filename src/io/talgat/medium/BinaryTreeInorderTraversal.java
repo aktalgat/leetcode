3c39b809c0665dfaec0ecf227360c0a5f3ef4809
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2026-02-08 16:41:05 -0600

    Binary Tree Inorder Traversal

---
+++ src/io/talgat/medium/BinaryTreeInorderTraversal.java
@@ -0,0 +1,27 @@
+package io.talgat.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * https://leetcode.com/problems/binary-tree-inorder-traversal
+ */
+public class BinaryTreeInorderTraversal {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            result.add(cur.val);
+            cur = cur.right;
+        }
+        return result;
+    }
+}
