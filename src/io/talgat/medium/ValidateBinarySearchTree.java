c0f8d93dcef6495f3d6b47d850d865614b8aa505
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2026-02-13 16:12:49 -0600

    Validate Binary Search Tree

---
+++ src/io/talgat/medium/ValidateBinarySearchTree.java
@@ -0,0 +1,24 @@
+package io.talgat.medium;
+
+/**
+ * https://leetcode.com/problems/validate-binary-search-tree
+ */
+public class ValidateBinarySearchTree {
+
+    public boolean isValidBST(TreeNode root) {
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean validate(TreeNode node, long min, long max) {
+        if (node == null) {
+            return true;
+        }
+
+        if (node.val <= min || node.val >= max) {
+            return false;
+        }
+
+        return validate(node.left, min, node.val) &&
+                validate(node.right, node.val, max);
+    }
+}
